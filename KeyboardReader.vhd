f90392d6e26f7ad45ace6addddd00ebe6ef9d79c
bmtavares <27867141+bmtavares@users.noreply.github.com>
2022-06-15 10:04:11 +0100

    Add clkDivider to KeyboardReader
    Fixes columns being selected after press, along with unstable presses

--- KeyboardReader.vhd
+++ KeyboardReader.vhd
@@ -3,10 +3,11 @@ use IEEE.std_logic_1164.all;
 
 ENTITY KeyboardReader IS
 	PORT (
-	clk, reset, TXclk	: IN  STD_LOGIC;
-	KEYPAD_LIN 			: IN  STD_LOGIC_VECTOR(3 downto 0);	
-	TXd, DBUG			: OUT STD_LOGIC;
-	KEYPAD_COL 			: OUT STD_LOGIC_VECTOR(2 downto 0)
+	clk,reset,TXclk	: IN  STD_LOGIC;
+	KEYPAD_LIN 		: IN  STD_LOGIC_VECTOR(3 downto 0);
+
+	TXd, DBUG		: OUT STD_LOGIC;
+	KEYPAD_COL 		: OUT STD_LOGIC_VECTOR(2 downto 0)
 	);
 END KeyboardReader;
 
@@ -14,32 +15,48 @@ Architecture accKeyboardReader of KeyboardReader is
 
 	COMPONENT KeyDecode
 		PORT (
-		clk, reset, kAck	: IN  STD_LOGIC;
-		KEYPAD_LIN 			: IN  STD_LOGIC_VECTOR(3 downto 0);	
-		kVal 				: OUT STD_LOGIC;
-		KEYPAD_COL 			: OUT STD_LOGIC_VECTOR(2 downto 0);
-		K					: OUT STD_LOGIC_VECTOR(3 downto 0)
+		clk,reset,kAck	: IN  STD_LOGIC;
+		KEYPAD_LIN 		: IN  STD_LOGIC_VECTOR(3 downto 0);
+
+		kVal 			: OUT STD_LOGIC;
+		KEYPAD_COL 		: OUT STD_LOGIC_VECTOR(2 downto 0);
+		K				: OUT STD_LOGIC_VECTOR(3 downto 0)
 		);
 	END COMPONENT;
 
 	COMPONENT KeyTransmitter
 		PORT (
-		clk, reset : in std_logic;
-		D : IN STD_LOGIC_vector(3 downto 0);
-		DAC : out STD_LOGIC;
-		DAV : in std_logic;
-		TXclk : in std_logic;
-		txD:out STD_LOGIC
+		clk,reset,
+		DAV,TXclk	: IN STD_LOGIC;
+		D 			: IN STD_LOGIC_VECTOR(3 downto 0);
+
+		DAC, txD	: OUT STD_LOGIC
 		);
 	END COMPONENT;
 
-	SIGNAL sig_DAC, sig_kVal 	: STD_LOGIC;
-	SIGNAL sig_D 				: STD_LOGIC_VECTOR(3 downto 0);
+	COMPONENT clkDIV
+		GENERIC (div : NATURAL);
+		PORT (
+			clk_in : IN STD_LOGIC;
+			clk_out : OUT STD_LOGIC
+		);
+	END COMPONENT;
+
+	SIGNAL sig_DAC,sig_kVal,sig_clkDivided	: STD_LOGIC;
+	SIGNAL sig_D 			: STD_LOGIC_VECTOR(3 downto 0);
 
 	BEGIN
+
+		uClkDIV:clkDIV
+		GENERIC MAP ( div => 500 )
+		PORT MAP (
+			clk_in 	=> clk,
+			clk_out	=> sig_clkDivided
+		);
+
 		uKeyDecode:KeyDecode
 			PORT MAP (
-			clk  		=> clk,
+			clk  		=> sig_clkDivided,
 			reset 		=> reset,
 			KEYPAD_LIN	=> KEYPAD_LIN,
 			KEYPAD_COL	=> KEYPAD_COL,
@@ -51,8 +68,8 @@ Architecture accKeyboardReader of KeyboardReader is
 		DBUG <= sig_kVal;
 
 		uKeyTransmitter:KeyTransmitter
-			PORT MAP(
-			clk		=> clk,
+			PORT MAP (
+			clk		=> sig_clkDivided,
 			reset	=> reset,
 			TXclk	=> TXclk,
 			DAV		=> sig_kVal,
