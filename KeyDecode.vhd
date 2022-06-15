638e794287e836c1b4137017c53f2483f2df418c
bmtavares <27867141+bmtavares@users.noreply.github.com>
2022-06-15 10:07:52 +0100

    Clean-up from local

--- KeyDecode.vhd
+++ KeyDecode.vhd
@@ -1,72 +1,63 @@
 LIBRARY IEEE;
 use IEEE.std_logic_1164.all;
 
-entity KeyDecode is
-port(
-	clk, reset : in std_logic;
-		KEYPAD_LIN : IN STD_LOGIC_vector(3 downto 0);	
-		KEYPAD_COL : OUT STD_LOGIC_vector(2 downto 0);
-	kVal : out STD_LOGIC;
-	kAck : in std_logic;
-	K: out std_logic_vector(3 downto 0)
+ENTITY KeyDecode IS
+	PORT(
+	clk, reset, kAck	: IN STD_LOGIC;
+	KEYPAD_LIN 			: IN STD_LOGIC_VECTOR(3 downto 0);
+
+	kVal 				: OUT STD_LOGIC;
+	KEYPAD_COL 			: OUT STD_LOGIC_VECTOR(2 downto 0);
+	K					: OUT STD_LOGIC_VECTOR(3 downto 0)
 	);
-end KeyDecode;
+END KeyDecode;
 
 
 Architecture accKeyDecode of KeyDecode is
 
-
 COMPONENT KeyControl
 	PORT (
-   		clk,kAck,kPress,reset : in STD_LOGIC;
-		kVal,kScan : out STD_LOGIC
+   	clk, kAck, kPress, reset	: IN STD_LOGIC;
+
+	kVal, kScan					: OUT STD_LOGIC
 	);
 END COMPONENT;
-
 
 COMPONENT KeyScan
-	port(
-		KEYPAD_LIN : IN STD_LOGIC_vector(3 downto 0);	
-		KEYPAD_COL : OUT STD_LOGIC_vector(2 downto 0);		
-		Kscan  : in std_logic;	
-		K: out std_logic_vector(3 downto 0);
-		clk, reset : in std_logic;
-		Kpress : out std_logic
+	PORT (
+	clk, reset, Kscan	: IN STD_LOGIC;
+	KEYPAD_LIN 			: IN STD_LOGIC_VECTOR(3 downto 0);	
+
+	Kpress 				: OUT STD_LOGIC;
+	KEYPAD_COL 			: OUT STD_LOGIC_VECTOR(2 downto 0);
+	K					: OUT STD_LOGIC_VECTOR(3 downto 0)
 	);
 END COMPONENT;
-
-
 
 
 SIGNAL sig_Kpress, sig_kScan : STD_LOGIC;
 
-
 BEGIN
-
-
-
-uKeyControl:KeyControl
-	PORT MAP(
-		clk    =>clk,
-		reset => reset,
-		kVal => kVal,
-		kAck => kAck,
-		kPress => sig_Kpress,
-		kScan => sig_kScan
+	uKeyControl:KeyControl
+		PORT MAP (
+		clk		=> clk,
+		reset	=> reset,
+		kVal 	=> kVal,
+		kAck 	=> kAck,
+		kPress	=> sig_Kpress,
+		kScan	=> sig_kScan
+		);
 		
-	);
-	
-	
-uKeyScan:KeyScan
-	PORT MAP(
-		clk    =>clk,
-		reset => reset,
-		KEYPAD_LIN => KEYPAD_LIN, 
-		KEYPAD_COL => KEYPAD_COL,
-		kPress => sig_Kpress,
-		Kscan => sig_kScan,
-		K => K
-	);	
-
+		
+	uKeyScan:KeyScan
+		PORT MAP (
+		clk			=> clk,
+		reset 		=> reset,
+		KEYPAD_LIN	=> KEYPAD_LIN, 
+		KEYPAD_COL	=> KEYPAD_COL,
+		kPress 		=> sig_Kpress,
+		Kscan 		=> sig_kScan,
+		K 			=> K
+		);	
 
 END accKeyDecode;
--- KeyScan.vhd
+++ KeyScan.vhd
@@ -3,80 +3,75 @@ USE ieee.std_logic_1164.all;
 use IEEE.std_logic_arith.ALL;
 
 ENTITY KeyScan IS
-PORT(	
-		KEYPAD_LIN : IN STD_LOGIC_vector(3 downto 0);	
-		KEYPAD_COL : OUT STD_LOGIC_vector(2 downto 0);		
-		K: out std_logic_vector(3 downto 0);
-		clk, reset : in std_logic;
-		Kscan  : in std_logic;	
-		Kpress : out std_logic
-		);
+	PORT (	
+		clk,reset,Kscan	: IN STD_LOGIC;
+		KEYPAD_LIN		: IN STD_LOGIC_VECTOR(3 downto 0);
+		
+		Kpress			: OUT STD_LOGIC;
+		KEYPAD_COL		: OUT STD_LOGIC_VECTOR(2 downto 0);		
+		K				: OUT STD_LOGIC_VECTOR(3 downto 0) := "0000"
+	);
 END KeyScan;
 
 ARCHITECTURE accKeyScan OF KeyScan IS
 
-signal  L0,L1,L2,L3 :  std_logic;
-signal Y1, Y0:  std_logic;
-signal nCLK ,nKscan:  std_logic;
-SIGNAL out_d : STD_LOGIC_vector (2 downto 0) ;
-SIGNAL Q : STD_LOGIC_vector (1 downto 0) ;
-signal sig0,sig1 : std_logic;
-signal sigY0,sigY1 : std_logic;
-
 	COMPONENT Counter_toT
 		PORT (
-	clk,clr,enable : in std_logic;
-	out_D : out std_logic_vector(1 downto 0)
+		clk,clr,enable	: IN STD_LOGIC;
+
+		out_D			: OUT STD_LOGIC_VECTOR(1 downto 0)
 		);
 	END COMPONENT;
 
 
-
+	SIGNAL L0,L1,L2,L3	: STD_LOGIC;
+	SIGNAL Y1, Y0		: STD_LOGIC;
+	SIGNAL nCLK ,nKscan	: STD_LOGIC;
+	SIGNAL out_d		: STD_LOGIC_VECTOR(2 downto 0);
+	SIGNAL Q			: STD_LOGIC_VECTOR(1 downto 0);
+	SIGNAL sig0,sig1	: STD_LOGIC;
+	SIGNAL sigY0,sigY1	: STD_LOGIC := '0';
 
 BEGIN
+	usCounter_toT:Counter_toT
+		PORT MAP(
+		clk => nCLK,
+		clr => '0',
+		enable => Kscan,
+		out_D => Q
+		);	
 
-usCounter_toT:Counter_toT PORT MAP(
-			clk => nCLK,
-			clr => '0',
-			enable => Kscan,
-			out_D => Q
-			);	
+	nCLK <= not clk ;
+	nKscan <= not Kscan;
+	L0 <= not KEYPAD_LIN(0) ;
+	L1 <= not KEYPAD_LIN(1) ;
+	L2 <= not KEYPAD_LIN(2) ;
+	L3 <= not KEYPAD_LIN(3) ;
 
+	--TODO: Não esquecer que os active lows são feitos dentro dos modulos
 
-nCLK <= not clk ;
-nKscan <= not Kscan;
-L0 <= not KEYPAD_LIN(0) ;
-L1 <= not KEYPAD_LIN(1) ;
-L2 <= not KEYPAD_LIN(2) ;
-L3 <= not KEYPAD_LIN(3) ;
+	--TODO: Refazer decoder
+	--dec
+	KEYPAD_COL(0) <= '0' when ( Q(0) = '0' and Q(1) = '0') else '1'; 
+	KEYPAD_COL(1) <= '0' when ( Q(0) = '1' and Q(1) = '0') else '1';  
+	KEYPAD_COL(2) <= '0' when ( Q(0) = '0' and Q(1) = '1') else '1'; 
+	--end dec
 
+	--TODO:Refazer priority enc
+	--encoder
+	Y0 <= L1 or L3;
+	Y1 <= L2 or L3 ;
+	--end encoder
 
+	--TODO: Meter isto como GS do penc
+	Kpress <= L0 or L1 or L2 or L3;
 
+	sigY0 <= Y0 when rising_edge(nKscan) else sigY0;
+	sigY1 <= Y1 when rising_edge(nKscan) else sigY1;
 
-
---dec
-KEYPAD_COL(0) <= '0' when ( Q(0) = '0' and Q(1) = '0') else '1'; 
-KEYPAD_COL(1) <= '0' when ( Q(0) = '1' and Q(1) = '0') else '1';  
-KEYPAD_COL(2) <= '0' when ( Q(0) = '0' and Q(1) = '1') else '1'; 
-
-
-                    
---encoder
-Y0 <= L1 or L3;
-Y1 <= L2 or L3 ;
---end encoder
-
-
-Kpress <= L0 or L1 or L2 or L3;
-
-sigY0 <= Y0 when rising_edge(nKscan) else sigY0;
-sigY1 <= Y1 when rising_edge(nKscan) else sigY1;
-
-K(0) <= sigY0;
-K(1) <= sigY1;
-K(2) <=  Q(0);
-K(3) <=  Q(1);
-
-
+	K(0) <= sigY0;
+	K(1) <= sigY1;
+	K(2) <=  Q(0);
+	K(3) <=  Q(1);
 
 END accKeyScan;
